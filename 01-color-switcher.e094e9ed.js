15fc5e4d9d75dff410ac3af2f6475796fe3b8908
YuliiaKoriavets <112959119+YuliiaKoriavets@users.noreply.github.com>
2022-12-28 04:56:46 +0000

    Deploying to gh-pages from @ YuliiaKoriavets/goit-js-hw-09@dd79b8af85b55505ed48a58c199d549aced0dafd 🚀

---
+++ 01-color-switcher.e094e9ed.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;e.disabled=!0,t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.e094e9ed.js.map
---
+++ 01-color-switcher.fd928e6c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;e.disabled=!0,t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,d=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.fd928e6c.js.map
